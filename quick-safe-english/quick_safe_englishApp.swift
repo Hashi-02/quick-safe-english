d7a20d9527b1d3ad119ed1be07606b0c5a58f836
hashi-02 <hassie0209@gmail.com>
2025-10-08 23:22:53 +0900

    Initial Commit

---
+++ quick-safe-english/quick_safe_englishApp.swift
@@ -0,0 +1,17 @@
+//
+//  quick_safe_englishApp.swift
+//  quick-safe-english
+//
+//  Created by hassie on 2025/10/08.
+//
+
+import SwiftUI
+
+@main
+struct quick_safe_englishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ quick-safe-englishUITests/quick_safe_englishUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  quick_safe_englishUITestsLaunchTests.swift
+//  quick-safe-englishUITests
+//
+//  Created by hassie on 2025/10/08.
+//
+
+import XCTest
+
+final class quick_safe_englishUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
